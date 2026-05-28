54f0b63af76ef72bcfaf8831b704ed68305cb0cd
milajotic <mijoa058@osloskolen.no>
2026-05-28 08:50:14 +0200

    more keyboard stuff

--- js/script.js
+++ js/script.js
@@ -2,6 +2,32 @@
 document.addEventListener("DOMContentLoaded", () => {
     createSquares();
 
+    const guessedWords = [[]];
+
+
+    const keys = document.querySelectorAll('.keyboard-row button')
+
+    for (let i = 0; i < keys.length; i++) {
+        keys[i].onclick = ({ target }) => {
+            const letter = target.getAttribute("data-key");
+
+            updateGuessedWords(letter);
+        };
+    }
+
+    function getCurrentWordArr() {
+        const numberOfGuessedWords = guessedWords.length;
+        return guessedWords[numberOfGuessedWords - 1];
+    }
+
+    function updateGuessedWords(letter) {
+        const currentWordArr = getCurrentWordArr();
+
+        if (currentWordArr && currentWordArr.length < 5) {
+            currentWordArr.push(letter);
+        }
+    }
+
     function createSquares() {
         const gameBoard = document.getElementById("board")
             
